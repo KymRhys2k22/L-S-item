bf2411618378231be146db4853054ba1ad5e78f4
KymRhys2@k22 <kymrhys@gmail.com>
2025-06-26 23:03:03 +0800

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <ProductForm />
-      <p className="text-2xl text-white">
-        this is variable: {import.meta.env.VITE_N8N_WEBHOOK_URL}
-      </p>
     </main>
   );
 }
