e951f8538a6134978f10f382c3e81e9fe7ef5c44
KymRhys2k22 <kymrhys@gnail.con>
2025-07-07 07:45:29 +0800

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ProductForm from "./components/ProductForm";
 function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
+      <h1>L & S Store</h1>
       <ProductForm />
     </main>
   );
